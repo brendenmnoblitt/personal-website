0d6f29c19996f44bdbc640fc7bb094682f4761b7
brendenmnoblitt <bnoblitt99@gmail.com>
2024-04-14 11:32:37 -0500

    refact

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__, static_folder="./frontend/build", static_url_path="/")
+app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return app.send_static_file('index.html')
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
---
+++ static/js/scripts.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const textToCode = document.getElementById("textToCode");
+    
+    textToCode.addEventListener("mouseenter", () => {
+        textToCode.innerHTML = "<code>hello, my name is @brendenmnoblitt</code>";
+    });
+
+    textToCode.addEventListener("mouseleave", () => {
+        textToCode.innerHTML = "Hello, my name is Brenden";
+    });
+});
+
+function toggleDarkMode() {
+    const toggleSwitch = document.querySelector('#flexSwitchCheckDefault');
+    const themeIcon = document.querySelector('#theme-icon');
+    const currentTheme = localStorage.getItem('theme');
+  
+    if (currentTheme) {
+      document.body.classList.toggle('dark-mode', currentTheme === 'dark');
+      toggleSwitch.checked = currentTheme === 'dark';
+      updateThemeIcon(themeIcon, currentTheme === 'dark');
+    }
+  
+    toggleSwitch.addEventListener('change', (e) => {
+      if (e.target.checked) {
+        document.body.classList.add('dark-mode');
+        localStorage.setItem('theme', 'dark');
+        updateThemeIcon(themeIcon, true);
+      } else {
+        document.body.classList.remove('dark-mode');
+        localStorage.setItem('theme', 'light');
+        updateThemeIcon(themeIcon, false);
+      }
+    });
+  }
+  
+  function updateThemeIcon(themeIcon, isDarkMode) {
+    themeIcon.classList.remove(isDarkMode ? 'fa-sun' : 'fa-moon');
+    themeIcon.classList.add(isDarkMode ? 'fa-moon' : 'fa-sun');
+  }
+  
+  toggleDarkMode();
